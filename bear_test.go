c285f63dfb2705e04825d28f96e0b25797c8c7ee
Pavel Derendyaev <dddpaul@gmail.com>
2019-02-23 13:05:34 +0300

    Remove crisp tests

--- bear_test.go
+++ bear_test.go
@@ -1,9 +1,6 @@
 package main
 
 import (
-	"encoding/json"
-	"fmt"
-	"reflect"
 	"strings"
 	"testing"
 
@@ -101,35 +98,4 @@ Empty Links [[]] should be ignored
 			Count: 1,
 		},
 	})
-}
-
-func TestMarshal(t *testing.T) {
-	note, _ := NewNote(strings.NewReader(`
-	# Title
-	#tag1 #tag2
-	What's difference between [[Thrift]] and [[GRPC]]
-	Or between [[GRPC]] and [[Thrift]] and [[SOAP]]
-	`)).Marshal()
-
-	expected := `{"title":"","tags":["tag1","tag2"],"links":[{"title":"Thrift","count":2},{"title":"GRPC","count":2},{"title":"SOAP","count":1}]}`
-
-	ok, _ := AreEqualJSON(expected, note)
-	assert.True(t, ok)
-}
-
-func AreEqualJSON(s1, s2 string) (bool, error) {
-	var o1 interface{}
-	var o2 interface{}
-
-	var err error
-	err = json.Unmarshal([]byte(s1), &o1)
-	if err != nil {
-		return false, fmt.Errorf("error mashalling string 1 :: %s", err.Error())
-	}
-	err = json.Unmarshal([]byte(s2), &o2)
-	if err != nil {
-		return false, fmt.Errorf("error mashalling string 2 :: %s", err.Error())
-	}
-
-	return reflect.DeepEqual(o1, o2), nil
 }
